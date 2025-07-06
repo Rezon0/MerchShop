bdf9769abb7f992544d2acf63448a160bc321ec3
Alex <alexrezon@mail.ru>
2025-07-06 10:37:00 +0300

    Create 20250706071338_ChangeDateOfBirthToDateTimeAgain.cs

---
+++ MerchShop.WebAPI/Migrations/20250706071338_ChangeDateOfBirthToDateTimeAgain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MerchShop.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDateOfBirthToDateTimeAgain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "DateOfBirth",
+                table: "Users",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
