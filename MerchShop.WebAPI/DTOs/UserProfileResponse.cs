784e7ce72a647c5444eca9bcba4754f57a5f4462
Alex <alexrezon@mail.ru>
2025-07-07 15:51:38 +0300

    Create UserProfileResponse.cs

---
+++ MerchShop.WebAPI/DTOs/UserProfileResponse.cs
@@ -0,0 +1,10 @@
+public class UserProfileResponse
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string? MiddleName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Phone { get; set; }
+    }
